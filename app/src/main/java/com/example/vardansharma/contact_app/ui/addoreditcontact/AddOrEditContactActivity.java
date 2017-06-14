353e185a4cd1c189021be7969914e7f2348fb4ca
vardansharma <vardan.sharma@bookmyshow.com>
2017-06-14 17:03:10 +0530

    basic ui for add and edit screen

--- app/src/main/java/com/example/vardansharma/contact_app/ui/addoreditcontact/AddOrEditContactActivity.java
+++ app/src/main/java/com/example/vardansharma/contact_app/ui/addoreditcontact/AddOrEditContactActivity.java
@@ -3,15 +3,33 @@ package com.example.vardansharma.contact_app.ui.addoreditcontact;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
 
 import com.example.vardansharma.contact_app.ContactsApp;
 import com.example.vardansharma.contact_app.R;
 import com.example.vardansharma.contact_app.base.BaseActivity;
 import com.example.vardansharma.contact_app.data.models.Contact;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class AddOrEditContactActivity extends BaseActivity implements AddOrEditCotactContract.Screen {
 
     public static final String EXTRA_CONTACT = "contact";
+    @BindView(R.id.add_edit_contact_toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.add_edit_contact_camera_btn)
+    ImageView cameraBtn;
+    @BindView(R.id.add_edit_contact_name)
+    TextInputEditText name;
+    @BindView(R.id.add_edit_contact_phone)
+    TextInputEditText phone;
+    @BindView(R.id.add_edit_contact_email)
+    TextInputEditText email;
     private AddOrEditContactComponent component;
 
     public static Intent createIntent(Context context, Contact contact) {
@@ -25,8 +43,24 @@ public class AddOrEditContactActivity extends BaseActivity implements AddOrEditC
         initDI();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_or_edit_contact);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(v -> onBackPressed());
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.add_or_edit_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_done) {
+            //do something here
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onDestroy() {
